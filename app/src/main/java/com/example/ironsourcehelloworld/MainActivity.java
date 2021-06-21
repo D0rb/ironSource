dd5f13e144c258b8d23b8b3a48dff0fbddfd0b9b
D0rb <dor.bismut@gmail.com>
2021-06-21 17:33:49 +0300

    :)

--- app/src/main/java/com/example/ironsourcehelloworld/MainActivity.java
+++ app/src/main/java/com/example/ironsourcehelloworld/MainActivity.java
@@ -11,70 +11,18 @@ import com.ironsource.mediationsdk.integration.IntegrationHelper;
 import com.ironsource.mediationsdk.logger.IronSourceError;
 import com.ironsource.mediationsdk.sdk.InterstitialListener;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements InterstitialListener{
     private final String APP_KEY = "85460dcd";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("Dor","onCreate");
-        IronSource.setInterstitialListener(new InterstitialListener() {
-            /**
-             Invoked when Interstitial Ad is ready to be shown after load function was called.
-             */
-            @Override
-            public void onInterstitialAdReady() {
-                Log.d("Dor","onInterstitialAdReady ");
-                IronSource.showInterstitial("Test?");
-            }
-            /**
-             invoked when there is no Interstitial Ad available after calling load function.
-             */
-            @Override
-            public void onInterstitialAdLoadFailed(IronSourceError error) {
-                Log.d("Dor","onInterstitialAdLoadFailed "+error);
-            }
-            /**
-             Invoked when the Interstitial Ad Unit is opened
-             */
-            @Override
-            public void onInterstitialAdOpened() {
-                Log.d("Dor","onInterstitialAdOpened ");
-            }
-            /*
-             * Invoked when the ad is closed and the user is about to return to the application.
-             */
-            @Override
-            public void onInterstitialAdClosed() {
-                Log.d("Dor","onInterstitialAdClosed ");
-            }
-            /**
-             * Invoked when Interstitial ad failed to show.
-             // @param error - An object which represents the reason of showInterstitial failure.
-             */
-            @Override
-            public void onInterstitialAdShowFailed(IronSourceError error) {
-                Log.d("Dor","onInterstitialAdShowFailed "+error);
-            }
-
-
-            /*
-             * Invoked when the end user clicked on the interstitial ad.
-             */
-            @Override
-            public void onInterstitialAdClicked() {
-                Log.d("Dor","onInterstitialAdClicked ");
-            }
-            /** Invoked right before the Interstitial screen is about to open.
-             NOTE - This event is available only for some of the networks. You should NOT treat this event as an interstitial impression,
-             but rather use InterstitialAdOpenedEvent */
-            @Override
-            public void onInterstitialAdShowSucceeded() {
-                Log.d("Dor","onInterstitialAdShowSucceeded");
-            }
-        });
-
-        IronSource.init(this, APP_KEY);
+        initIronSource(APP_KEY);
+    }
+    private void initIronSource(String APPKEY) {
+        IronSource.setInterstitialListener(this);
+        IronSource.init(this, APPKEY);
         IronSource.loadInterstitial();
     }
     protected void onResume() {
@@ -86,5 +34,41 @@ public class MainActivity extends AppCompatActivity {
         super.onPause();
         IronSource.onPause(this);
         Log.d("Dor","onPause");
+    }
+
+    @Override
+    public void onInterstitialAdReady() {
+        Log.d("Dor","onInterstitialAdReady ");
+        IronSource.showInterstitial("Test?");
+    }
+
+    @Override
+    public void onInterstitialAdLoadFailed(IronSourceError ironSourceError) {
+        Log.d("Dor","onInterstitialAdLoadFailed "+ironSourceError);
+    }
+
+    @Override
+    public void onInterstitialAdOpened() {
+        Log.d("Dor","onInterstitialAdOpened ");
+    }
+
+    @Override
+    public void onInterstitialAdClosed() {
+        Log.d("Dor","onInterstitialAdClosed");
+    }
+
+    @Override
+    public void onInterstitialAdShowSucceeded() {
+        Log.d("Dor","onInterstitialAdShowSucceeded");
+    }
+
+    @Override
+    public void onInterstitialAdShowFailed(IronSourceError ironSourceError) {
+        Log.d("Dor","onInterstitialAdShowFailed "+ironSourceError);
+    }
+
+    @Override
+    public void onInterstitialAdClicked() {
+        Log.d("Dor","onInterstitialAdClicked");
     }
 }
